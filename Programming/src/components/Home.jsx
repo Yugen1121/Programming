0afd40bd841eabe97a08a74e1afb04eddd5c1a61
Yugen1121 <yugenlimbu777@gmail.com>
2025-04-22 17:16:12 +0100

    added a home component

--- Programming/src/components/Home.jsx
+++ Programming/src/components/Home.jsx
@@ -1,6 +1,29 @@
+import ApiFetch from "./ApiFetch"
+import { useEffect, useState } from "react";
+
 function Home(){
 
+    let [response, updateResponse] = useState(null);
+    let [error, updateError] = useState(null);
+    useEffect(()=>{
+        ApiFetch("https://newsapi.org/v2/everything?q=apple&from=2025-04-21&to=2025-04-21&sortBy=popularity&apiKey=22f63d877828444b9ba61543b8e2b3fe")
+        .then(data => {
+            updateResponse(data)
+        })
+        .catch(err => {
+            updateError(err.message);
+        })
+    },[])
     
+
+    return <>
+    <p>
+        {response ? (
+            <div>{JSON.stringify(response, null, 2)}</div>)
+            : <div>Error: {error}</div>
+        }
+    </p>
+    </>
 
 }
 
