65897c07f308f97088680c335de52b71169ab8de
Yugen1121 <yugenlimbu777@gmail.com>
2025-04-23 18:09:21 +0100

    Modified to accomodate chnages

--- Programming/src/App.jsx
+++ Programming/src/App.jsx
@@ -1,14 +1,13 @@
 import { useState } from 'react'
 import viteLogo from '/vite.svg'
 import Nav from "./components/Nav"
-import BMI from './components/BMI'
-import CenterContainer from './components/Container'
-import BreathingHelper from './components/Breath'
-import Profile from './components/Profile'
+import Symptoms from './components/Symptoms'
 
 function App() {
+  
   return <>
     <Nav></Nav> 
+    <Symptoms />
   </>
 }
 
