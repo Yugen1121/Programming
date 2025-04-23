553eb1fc7562e0edcbce1b3dd9c741e2ed6081b8
Yugen1121 <yugenlimbu777@gmail.com>
2025-04-23 18:06:16 +0100

    Modified the component to accomodate the apifetch changes

--- Programming/src/components/Home.jsx
+++ Programming/src/components/Home.jsx
@@ -1,26 +1,37 @@
 import ApiFetch from "./ApiFetch"
 import { useEffect, useState } from "react";
-
+//"https://newsapi.org/v2/everything?q=apple&from=2025-04-21&to=2025-04-21&sortBy=popularity&apiKey=22f63d877828444b9ba61543b8e2b3fe",
 function Home(){
 
     let [response, updateResponse] = useState(null);
     let [error, updateError] = useState(null);
+    let [loading, updateLoading] = useState(true)
     useEffect(()=>{
-        ApiFetch("https://newsapi.org/v2/everything?q=apple&from=2025-04-21&to=2025-04-21&sortBy=popularity&apiKey=22f63d877828444b9ba61543b8e2b3fe")
+        ApiFetch("https://newsapi.org/v2/everything?q=fitness&from=2025-04-22&to=2025-04-22&sortBy=popularity", 
+            {
+          headers: {
+            'Authorization': `Bearer 22f63d877828444b9ba61543b8e2b3fe`, 
+          }
+        })
         .then(data => {
-            updateResponse(data)
+            updateLoading(false);
+            updateResponse(data);
         })
         .catch(err => {
+            updateLoading(false);
             updateError(err.message);
+            
         })
     },[])
     
 
     return <>
     <p>
-        {response ? (
+        {loading? (<div>Loading...</div>) 
+        : (response 
+        ? (
             <div>{JSON.stringify(response, null, 2)}</div>)
-            : <div>Error: {error}</div>
+            : <div>Error: {error}</div>)
         }
     </p>
     </>
