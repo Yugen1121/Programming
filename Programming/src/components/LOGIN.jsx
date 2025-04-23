b1b4714731d75db6c4c8bfd53e3895f2c91143e5
Yugen1121 <yugenlimbu777@gmail.com>
2025-04-23 18:07:49 +0100

    Added the function to log in in the website

--- Programming/src/components/LOGIN.jsx
+++ Programming/src/components/LOGIN.jsx
@@ -6,6 +6,22 @@ import image2 from "./assets/wow.png";
 
 function Login() {
 
+    const [email, updateEmil] = useState("")
+    const [password, updatePassword] = useState("")
+    const [errir, updateError] = useState(false)
+    
+    const handleEmail = (event)=>{
+        updateEmil(event.target.value);
+    }
+
+    const handlePassword = (event)=>{
+        event.preventDefault()
+        updatePassword(event.target.value);
+    }
+
+    const handleSubmit = ()=>{
+
+    }
 
     return (
         <>
@@ -13,16 +29,16 @@ function Login() {
         <Form>
         <Form.Group className="mb-3" controlId="formBasicEmail">
             <Form.Label>Email:</Form.Label>
-            <Form.Control  type="text" step="any" placeholder="Email" />
+            <Form.Control onChange={handleEmail}  type="text" step="any" placeholder="Email" />
             <Form.Text className="text-muted">
             </Form.Text>
         </Form.Group>
 
         <Form.Group className="mb-3" controlId="formBasicPassword">
             <Form.Label>password:</Form.Label>
-            <Form.Control type="password" step="any" placeholder="Password" />
+            <Form.Control onChange={handlePassword} type="password" step="any" placeholder="Password" />
         </Form.Group>
-        <Button variant="primary" type="submit" >
+        <Button onSubmit={handleSubmit} variant="primary" type="submit" >
             Submit
         </Button>
         </Form>
